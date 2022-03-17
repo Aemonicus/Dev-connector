98d5948cdce54559ca25e7669c4bd7ad57ee3128
Florent VICENTE <vicentegroupe@gmail.com>
2022-03-17 14:35:31 +0100

    user registration

--- InfosProject.md
+++ InfosProject.md
@@ -1,3 +1,15 @@
 # Infos du projet
 
 J'ai utilisé les versions identiques des packages à celles du tuto, donc si les libs ne sont pas à jour, c'est normal. J'ai privilégié la stabilité et le bon fonctionnement du projet aux prises de têtes avec les dernières versions touzolitoutbo
+
+## Erreurs Serveurs
+
+- Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+
+| Dans ma route users.js, j'avais oublié de mettre un return dans les check d'erreurs de if(){}. A chaque fois que l'on fait un check de validation et que l'on veut que l'erreur renvoie un message à travers une response (ex : res.send()), il faut mettre un return dans le if sinon le code continue (même en cas d'erreur) son exécution jusqu'en bas où il peut rencontrer un second res.send().
+
+```js
+if (user) {
+  return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+}
+```
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router()
-const { check, validationResult } = require('express-validator/check')
+const gravatar = require("gravatar")
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+
+const User = require('../../models/User')
 
 // @route   POST api/users
 // @desc    Register user
@@ -13,15 +17,48 @@ router.post("/", [
     .isEmail(),
   check("password", "Password is required witha minimum of six characters")
     .isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() })
-  } else {
-    console.log(req.body)
-    const name = req.body.name
-    res.send(name)
   }
+
+  const { name, email, password } = req.body
+
+  try {
+
+    let user = await User.findOne(({ email }));
+
+    if (user) {
+      return res.status(400).json({ errors: [ { msg: "User already exists" } ] })
+    };
+
+    const avatar = gravatar.url(email, {
+      s: "200",
+      r: "pg",
+      d: "mm"
+    });
+
+    user = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.send("User registered");
+
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("Server error")
+  }
+
 
 })
 
