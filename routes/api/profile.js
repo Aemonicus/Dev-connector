fbf52d7e25f662c8cca711c606998c11cfc9f9ff
Florent VICENTE <vicentegroupe@gmail.com>
2022-03-18 16:34:58 +0100

    Get github repos for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router()
 const auth = require("../../middleware/auth")
 const { check, validationResult } = require('express-validator')
@@ -321,5 +323,33 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     res.status(400).json({ msg: "Profile not found" })
   }
 })
+
+
+// Route utilisée pour récupérer les repos github, il faut vérifier si l'uri de la requête est toujours à jour (tuto ancien) et poser le githubClientId et githubSecret récupérable sur le site de github
+// // @route   GET api/profile/github/:username
+// // @desc    Get user repos from Github
+// // @access  Public
+// router.get("/github/:username", async (req, res) => {
+//   try {
+//     const options = {
+//       uri: `https://api/github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}`,
+//       method: "GET",
+//       headers: { "user-agent": "node.js" }
+//     };
+
+//     request(options, (error, response, body) => {
+//       if (error) console.error(error);
+
+//       if (response.statusCode !== 200) {
+//         return res.status(404).json({ msg: "No Github Profile found" })
+//       }
+
+//       res.json(JSON.parse(body));
+//     })
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("Server Error");
+//   }
+// })
 
 module.exports = router
