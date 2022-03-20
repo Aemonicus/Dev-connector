51151c767a1043cc97fc4f765abfcf36f638618b
Florent VICENTE <vicentegroupe@gmail.com>
2022-03-20 16:23:03 +0100

    redux store

--- client/src/App.js
+++ client/src/App.js
@@ -18,50 +18,50 @@ import Login from './components/auth/Login';
 // import PrivateRoute from './components/routing/PrivateRoute';
 // import { LOGOUT } from './actions/types';
 
-// // Redux
-// import { Provider } from 'react-redux';
-// import store from './store';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 // import { loadUser } from './actions/auth';
 // import setAuthToken from './utils/setAuthToken';
 
 const App = () => {
   return (
-    // <Provider store={store}>
-    <Router>
-      <Navbar />
-      {/* <Alert /> */}
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} />
-        {/* <Route path="profiles" element={<Profiles />} /> */}
-        {/* <Route path="profile/:id" element={<Profile />} /> */}
-        {/* <Route
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        {/* <Alert /> */}
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          {/* <Route path="profiles" element={<Profiles />} /> */}
+          {/* <Route path="profile/:id" element={<Profile />} /> */}
+          {/* <Route
           path="dashboard"
           element={<PrivateRoute component={Dashboard} />}
         /> */}
-        {/* <Route
+          {/* <Route
           path="create-profile"
           element={<PrivateRoute component={ProfileForm} />}
         /> */}
-        {/* <Route
+          {/* <Route
           path="edit-profile"
           element={<PrivateRoute component={ProfileForm} />}
         /> */}
-        {/* <Route
+          {/* <Route
           path="add-experience"
           element={<PrivateRoute component={AddExperience} />}
         /> */}
-        {/* <Route
+          {/* <Route
           path="add-education"
           element={<PrivateRoute component={AddEducation} />}
         /> */}
-        {/* <Route path="posts" element={<PrivateRoute component={Posts} />} /> */}
-        {/* <Route path="posts/:id" element={<PrivateRoute component={Post} />} /> */}
-        {/* <Route path="/*" element={<NotFound />} /> */}
-      </Routes>
-    </Router>
-    // </Provider>
+          {/* <Route path="posts" element={<PrivateRoute component={Posts} />} /> */}
+          {/* <Route path="posts/:id" element={<PrivateRoute component={Post} />} /> */}
+          {/* <Route path="/*" element={<NotFound />} /> */}
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
