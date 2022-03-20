5782a1255f6e70883f70c2851e7fc8c27efd6715
Florent VICENTE <vicentegroupe@gmail.com>
2022-03-20 12:31:28 +0100

    end of back end part, start front end with react install, clean up && first components

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
+}
+
+export default App;
