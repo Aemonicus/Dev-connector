a664b314a883d80433a8b2dcff3cee1ae3f68a9c
Florent VICENTE <vicentegroupe@gmail.com>
2022-03-21 14:18:41 +0100

    CreateProfile component

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,5 @@
 import api from '../utils/api';
-import { setAlerts } from "./alert";
+import { setAlert } from "./alert";
 
 import {
   GET_PROFILE,
@@ -29,3 +29,36 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// Create or update profile
+export const createProfile =
+  (formData, navigate, edit = false) =>
+    async (dispatch) => {
+      try {
+        const res = await api.post('/profile', formData);
+
+        dispatch({
+          type: GET_PROFILE,
+          payload: res.data
+        });
+
+        dispatch(
+          setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success')
+        );
+
+        if (!edit) {
+          navigate('/dashboard');
+        }
+      } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+          errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+          type: PROFILE_ERROR,
+          payload: { msg: err.response.statusText, status: err.response.status }
+        });
+      }
+    };
